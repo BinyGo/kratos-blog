846e3b165795e7b208a6fdaba02a8740090b25c8
BinyGo <58040087@qq.com>
2022-04-22 14:16:12 +0800

    blogV1.0

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,6 +1,19 @@
 package service
 
-import "github.com/google/wire"
+import (
+	pb "kratos-blog/api/blog/v1"
+	"kratos-blog/internal/biz"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
 
 // ProviderSet is service providers.
-var ProviderSet = wire.NewSet(NewGreeterService)
+var ProviderSet = wire.NewSet(NewBlogService)
+
+type BlogService struct {
+	pb.UnimplementedBlogServer
+	log *log.Helper
+
+	article *biz.ArticleUsecase
+}
