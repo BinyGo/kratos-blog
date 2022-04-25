f8954e9682d789f1cd50891f826967fa0c546fe9
BinyGo <58040087@qq.com>
2022-04-25 14:00:55 +0800

    feat:monitor-prometheus-metrics

--- go.mod
+++ go.mod
@@ -23,5 +23,7 @@ require (
 require (
 	github.com/casbin/casbin/v2 v2.42.0
 	github.com/casbin/xorm-adapter/v2 v2.5.1
+	github.com/go-kratos/kratos/contrib/metrics/prometheus/v2 v2.0.0-20220422120629-fbf7855cf262
+	github.com/prometheus/client_golang v1.9.0
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 )
--- internal/server/http.go
+++ internal/server/http.go
@@ -8,9 +8,11 @@ import (
 
 	myCasbin "kratos-blog/internal/pkg/casbin"
 
+	prom "github.com/go-kratos/kratos/contrib/metrics/prometheus/v2"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/metrics"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/middleware/selector"
 	"github.com/go-kratos/kratos/v2/middleware/tracing"
@@ -19,6 +21,30 @@ import (
 	"github.com/go-kratos/swagger-api/openapiv2"
 	jwt4 "github.com/golang-jwt/jwt/v4"
 	"github.com/gorilla/handlers"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	// Name is the name of the compiled software.
+
+	// Version is the version of the compiled software.
+	// Version = "v1.0.0"
+
+	_metricSeconds = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "server",
+		Subsystem: "requests",
+		Name:      "duration_sec",
+		Help:      "server requests duration(sec).",
+		Buckets:   []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.250, 0.5, 1},
+	}, []string{"kind", "operation"})
+
+	_metricRequests = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "client",
+		Subsystem: "requests",
+		Name:      "code_total",
+		Help:      "The total number of processed requests",
+	}, []string{"kind", "operation", "code", "reason"})
 )
 
 func NewWhiteListMatcher() selector.MatchFunc {
@@ -36,13 +62,8 @@ func NewWhiteListMatcher() selector.MatchFunc {
 
 // NewHTTPServer new a HTTP server.
 func NewHTTPServer(c *conf.Server, ac *conf.Auth, cd *conf.Data, blog *service.BlogService, auth *service.AuthService, logger log.Logger) *http.Server {
-	// m, _ := model.NewModelFromFile("../../configs/authz/authz_model.conf")
-	// a := fileAdapter.NewAdapter("../../configs/authz/authz_policy.csv")
-	// a, err := entAdapter.NewAdapter("mysql", "root:123456@tcp(127.0.0.1:3306)/kratos-blog?parseTime=True")
-	// if err != nil {
-	// 	fmt.Println("-----------------------------")
-	// 	fmt.Println(err)
-	// }
+
+	prometheus.MustRegister(_metricSeconds, _metricRequests)
 
 	var opts = []http.ServerOption{
 		http.Middleware(
@@ -62,6 +83,10 @@ func NewHTTPServer(c *conf.Server, ac *conf.Auth, cd *conf.Data, blog *service.B
 				),
 				myCasbin.Server(cd),
 			).Match(NewWhiteListMatcher()).Build(),
+			metrics.Server(
+				metrics.WithSeconds(prom.NewHistogram(_metricSeconds)),
+				metrics.WithRequests(prom.NewCounter(_metricRequests)),
+			),
 		),
 		http.Filter(handlers.CORS(
 			handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
@@ -79,8 +104,12 @@ func NewHTTPServer(c *conf.Server, ac *conf.Auth, cd *conf.Data, blog *service.B
 		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
 	}
 	srv := http.NewServer(opts...)
+	//http://localhost:8000/q/swagger-ui
 	openAPIhandler := openapiv2.NewHandler()
 	srv.HandlePrefix("/q/", openAPIhandler)
+
+	srv.Handle("/metrics", promhttp.Handler())
+
 	v1.RegisterBlogHTTPServer(srv, blog)
 	v1.RegisterAuthHTTPServer(srv, auth)
 	return srv
